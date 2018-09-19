842d87c37cae59a5761a1c83f726d017a1872540
Owen Merry <owenmerry@Owens-MacBook-Air.local>
2018-09-19 21:53:01 +0100

    added in initial setup with links and loops

--- src/App.js
+++ src/App.js
@@ -3,16 +3,72 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+
+  //react functions
+
+  constructor(props) {
+    super(props);
+
+    //default values
+    this.state = {
+      data: [],
+      isLoading: true,
+    };
+
+    // bind functions
+    this.getData = this.getData.bind(this);
+
+
+  }
+
+  componentDidMount() {
+
+    //get data
+    this.getData();
+  }
+
+
+  // my functions
+
+  getData(){
+    //defaults
+    var apiLink = "";
+
+    //variables
+    apiLink = '/api/projects.json';
+    
+    //get data
+    fetch(apiLink)
+      .then(response => response.json())
+      .then(data => this.setState({ data ,isLoading:false }));
+
+  }
+
+
+
+
   render() {
+
+    //if loading
+    if (this.state.isLoading) {
+      return <p>Loading ...</p>;
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+
+        {this.state.data.projects.map(project =>
+            <div>
+            <h2>{project.title}</h2>
+            <ul>
+            {project.links.map(link =>
+              <li><a target="_blank" href={link.link}>{link.text}</a></li>
+            )}
+            </ul>
+            </div>
+          )}
       </div>
     );
   }
